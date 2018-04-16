4bd4118f1a6576f204217405d123b8cd5b9ca6b1
Jessika Costa Cardoso <jessika.cost@gmail.com>
2018-04-15 21:35:39 -0300

    adding version 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,83 @@ let btnNao = document.getElementById("btnNao").value;
 let resultado = [];
 
 
+let imageArray = [
+    '01-sim.png',
+    '02-sim.png',
+    '03-sim.png',
+    '04-sim.png',
+    '05-nao.png',
+    '06-nao.png',
+    '07-sim.png',
+    '08-sim.png',
+    '09-nao.png',
+    '10-sim.png'
+]
+
+let i = 0;
+let totalImagens = 8;//***** criamos um array para contar as imagens
+
+
+// Aqui está colocando a imageArray[0] que é da pergunta-01:
+document.getElementById('image').src = 'assets/img/' + imageArray[i];
+
+// A partir do momento que clica:
+
+
 //Guardando a resposta num array e direcionando para a proxima pagina.
 let guardarSim = function(){
-    if(btnSim === "true" || btnSim === "false"){
+    if(i <=totalImagens){
+        i++
         let valor = resultado.push(btnSim);
+        //i; i <=totalImagens; i++
+         // i = 1, então a imgArray[1] vai ser a pergunta-02
+        document.querySelector('#image').src = 'assets/img/' + imageArray[i];
         console.log (valor);
+    }
+    else{
+       // window.location.replace("resultado.html");
+       document.write(resultado);
     }
 }
 
 
 let guardarNao = function(){
-    if(btnNao === "true" || btnNao === "false"){
+    if(i <=totalImagens){
+        i++
         let valor = resultado.push(btnNao);
+        //i; i <=totalImagens; i++
+         // i = 1, então a imgArray[1] vai ser a pergunta-02
+        document.querySelector('#image').src = 'assets/img/' + imageArray[i];
         console.log (valor);
+    }
+    else{
+       // window.location.replace("resultado.html");
+    document.write(resultado);
     }
 }
 
 console.log(resultado);
+
+
+let respostasCorretas= new Array();
+
+respostasCorretas[0]="sim";
+respostasCorretas[1]="sim";
+respostasCorretas[2]="sim";
+respostasCorretas[3]="sim";
+respostasCorretas[4]="sim";
+respostasCorretas[5]="nao";
+respostasCorretas[6]="nao";
+respostasCorretas[7]="sim";
+respostasCorretas[8]="sim";
+respostasCorretas[9]="nao";
+respostasCorretas[10]="sim";
+
+console.log(respostasCorretas);
+
+
+
+
+//console.log(resultado);
+
+
