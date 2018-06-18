7979d3f91ad38a3a918a12a8da859affa0a70309
jessikacostac <jessika.cost@gmail.com>
2018-06-18 12:52:48 -0300

    update v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,49 @@
-//1) criando um array para guardar as imagens
-let imagens = [
-    "01-sim.png",
-    "02-sim.png",
-    "03-sim.png",
-    "04-sim.png",
-    "05-sim.png",
-    "06-sim.png",
-    "07-sim.png",
-    "08-sim.png",
-    "09-sim.png",
-    "10-sim.png",
-];
-console.log(imagens);
+//Acesso ao botao Sim
+let btnSim = document.getElementById("btnSim").value;
+let btnNao = document.getElementById("btnNao").value;
+let resultado = [];
 
 
-//2) Criando um contador 
-let contador = 0;
+let imageArray = [
+    '01-sim.png',
+    '02-sim.png',
+    '03-sim.png',
+    '04-sim.png',
+    '05-nao.png',
+    '06-nao.png',
+    '07-sim.png',
+    '08-sim.png',
+    '09-nao.png',
+    '10-sim.png'
+]
+
+let i = 0;
+// Aqui está colocando a imageArray[0] que é da pergunta-01:
+document.getElementById('image').src = 'assets/img/' + imageArray[i];
+
+// A partir do momento que clica:
 
 
-//3) Acesso888888 ------  . concatena methodo --------  e + concatena string
-document.getElementById("passarImg").src="./assets/img/" + imagens[contador];
-
-
-//4) Function
-function mudarImg (){
-    contador++ // incrmentando cada clique
-    document.querySelector("#imagens").src = "./assets/"+ imagens[contador]
+//Guardando a resposta num array e direcionando para a proxima pagina.
+let guardarSim = function(){
+    if(btnSim === "true" || btnSim === "false"){
+        let valor = resultado.push(btnSim);
+        i++ // i = 1, então a imgArray[1] vai ser a pergunta-02
+        document.querySelector('#image').src = 'assets/img/' + imageArray[i];
+        console.log (valor);
+    }
 }
 
-//5)
-document.querySelectorAll(".botao").forEach(function(current, idx, callback){
-    current.addEventListener("click", onclick)
-})
+
+let guardarNao = function(){
+    if(btnNao === "true" || btnNao === "false"){
+        let valor = resultado.push(btnNao);
+        i++ // i = 1, então a imgArray[1] vai ser a pergunta-02
+        document.querySelector('#image').src = 'assets/img/' + imageArray[i];
+        console.log (valor);
+    }
+}
+
+console.log(resultado);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Acesso ao botao Sim
-// let btnSim = document.getElementById("btnSim").value;
-// let btnNao = document.getElementById("btnNao").value;
-// let resultado = [];
-
-
-// //Guardando a resposta num array e direcionando para a proxima pagina.
-// let guardarSim = function(){
-//     if(btnSim === "true" || btnSim === "false"){
-//         let valor = resultado.push(btnSim);
-//         console.log (valor);
-//     }
-// }
-
-
-// let guardarNao = function(){
-//     if(btnNao === "true" || btnNao === "false"){
-//         let valor = resultado.push(btnNao);
-//         console.log (valor);
-//     }
-// }
-
-// console.log(resultado);
