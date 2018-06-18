a2a54eb9215845e8729103c18c5118f825297542
jessikacostac <jessika.cost@gmail.com>
2018-06-18 12:55:42 -0300

    update v5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Acesso ao botao Sim
+//Acesso ao botao Sim e Não
 let btnSim = document.getElementById("btnSim").value;
 let btnNao = document.getElementById("btnNao").value;
 let resultado = [];
@@ -15,27 +15,28 @@ let imageArray = [
     '08-sim.png',
     '09-nao.png',
     '10-sim.png'
-]
+    //'11-face-feliz.png'
+];
 
 let i = 0;
-let totalImagens = 8;//***** criamos um array para contar as imagens
+let totalImagens = imageArray.length;//***** criamos um array para contar as imagens-- **substituir por .length
 
 
 // Aqui está colocando a imageArray[0] que é da pergunta-01:
-document.getElementById('image').src = 'assets/img/' + imageArray[i];
-
+//document.getElementById('image').src = 'assets/img/' + imageArray[i];--**nao precisa
 // A partir do momento que clica:
 
 
-//Guardando a resposta num array e direcionando para a proxima pagina.
+//Guardando a resposta num array e direcionando para a proxima img.
 let guardarSim = function(){
     if(i <=totalImagens){
-        i++
+        
         let valor = resultado.push(btnSim);
+        i++;
         //i; i <=totalImagens; i++
          // i = 1, então a imgArray[1] vai ser a pergunta-02
         document.querySelector('#image').src = 'assets/img/' + imageArray[i];
-        console.log (valor);
+        console.log (resultado);
     }
     else{
        // window.location.replace("resultado.html");
@@ -43,15 +44,15 @@ let guardarSim = function(){
     }
 }
 
-
 let guardarNao = function(){
-    if(i <=totalImagens){
-        i++
+    if(i <totalImagens){
+        
         let valor = resultado.push(btnNao);
+        i++
         //i; i <=totalImagens; i++
          // i = 1, então a imgArray[1] vai ser a pergunta-02
         document.querySelector('#image').src = 'assets/img/' + imageArray[i];
-        console.log (valor);
+        console.log (resultado);
     }
     else{
        // window.location.replace("resultado.html");
@@ -59,24 +60,24 @@ let guardarNao = function(){
     }
 }
 
-console.log(resultado);
+//console.log(resultado);
 
 
 let respostasCorretas= new Array();
+
 
 respostasCorretas[0]="sim";
 respostasCorretas[1]="sim";
 respostasCorretas[2]="sim";
 respostasCorretas[3]="sim";
-respostasCorretas[4]="sim";
+respostasCorretas[4]="nao";
 respostasCorretas[5]="nao";
-respostasCorretas[6]="nao";
+respostasCorretas[6]="sim";
 respostasCorretas[7]="sim";
-respostasCorretas[8]="sim";
-respostasCorretas[9]="nao";
-respostasCorretas[10]="sim";
+respostasCorretas[8]="nao";
+respostasCorretas[9]="sim";
 
-console.log(respostasCorretas);
+//console.log(respostasCorretas);
 
 
 
